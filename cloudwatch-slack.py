158e788f523e7ac850fcb51e27ac9ed416fd304d
ryo00123 <ten3mm8apppoe@gmail.com>
2022-02-06 00:16:19 +0900

    cloudwatch-slack

---
+++ cloudwatch-slack.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+import logging
+import os
+
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+
+HOOK_URL = os.environ['hookurl']
+SLACK_CHANNEL = os.environ['slackChannel']
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info("Event: " + str(event))
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    logger.info("Message: " + str(message))
+
+    alarm_name = message['AlarmName']
+    #old_state = message['OldStateValue']
+    new_state = message['NewStateValue']
+    reason = message['NewStateReason']
+    alarm_description = message['AlarmDescription']
+
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        'text': "<!here> \nアラーム名: %s\nステータス: %s\nアラーム理由: %s\n説明: %s" % (alarm_name, new_state, reason, alarm_description)
+    }
+
+    req = Request(HOOK_URL, json.dumps(slack_message).encode('utf-8'))
+    try:
+        response = urlopen(req)
+        response.read()
+        logger.info("Message posted to %s", slack_message['channel'])
+    except HTTPError as e:
+        logger.error("Request failed: %d %s", e.code, e.reason)
+    except URLError as e:
+        logger.error("Server connection failed: %s", e.reason)
